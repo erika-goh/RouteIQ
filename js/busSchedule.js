4b58119ee5240e50373082c75619fa1756e7d43d
Erika <93851124+erika-goh@users.noreply.github.com>
2026-01-10 23:54:38 -0500

    Create busSchedule.js

---
+++ js/busSchedule.js
@@ -0,0 +1,9 @@
+const goBusSchedules = {
+};
+
+function handleTravelModeChange() 
+function updateBusSchedule() 
+
+document.getElementById('bus-schedule').addEventListener('change', (e) => {
+    selectedBusTime = e.target.value;
+});
